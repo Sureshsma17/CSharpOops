8e610c40c7a90c825555cce875cdb52f77a18d7a
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-11 12:08:33 +0530

    commit

---
+++ PrintNameWiithparameterWithoutReturnValue.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharpOops
+//{
+//    internal class PrintNameWiithparameterWithoutReturnValue
+//    {
+//          class Myclas
+//        {
+//            public void PrintName(string name) 
+//            {
+//                Console.WriteLine("My name is " + name);
+//            }
+
+//            static void Main(string[] args)
+//            {
+//                Myclas myclas = new Myclas();
+//                myclas.PrintName("Suresh");
+//            }
+//        }
+
+//    }
+//}
---
+++ PrintNameWithotReturnWithoutParameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOops
+{
+    internal class PrintNameWithotReturnWithoutParameter
+    {
+        class Myclass
+        {
+            public void PrintName()
+            {
+                Console.WriteLine("Hello, My Name is Suresh");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Myclass myclass = new Myclass();
+            myclass.PrintName(); 
+        }
+    }
+}
