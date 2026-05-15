6b902d99292c8a1e36db508d1774311c06fe2dd1
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-15 12:08:05 +0530

    commit

---
+++ ParameterizedCarExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOops
+{
+    class Car
+    {
+        string Model;
+        string Color;
+        short Year;
+
+        public Car(string model, string color, short year)
+        {
+            Model = model;
+            Color = color;
+            Year = year;
+
+        }
+        public void DisplayCarDetails()
+        {
+            Console.WriteLine("Model of Car is: " + Model);
+            Console.WriteLine("Color of Car is: " + Color);
+            Console.WriteLine("Manufacturing Year of Car is: " + Year);
+        }
+
+    }
+    internal class ParameterizedCarExample
+    {
+        static void Main(string[] args)
+        {
+            {
+                Car objcar = new Car("Toyota Vellfire", "White", 2026);
+                objcar.DisplayCarDetails();
+            }
+        }
+    }
+}
+        
--- ParameterizedDataBaseConnection.cs
+++ ParameterizedDataBaseConnection.cs
@@ -1,51 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.SqlClient;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpOops
-{
-    class DataBaseConnection
-    {
-        string connectionstring;
-        SqlConnection connection;
-        public DataBaseConnection(string connstring)
-        {
-            connectionstring = connstring;
-            connection=new SqlConnection(connectionstring);
+//namespace CSharpOops
+//{
+//    class DataBaseConnection
+//    {
+//        string connectionstring;
+//        SqlConnection connection;
+//        public DataBaseConnection(string connstring)
+//        {
+//            connectionstring = connstring;
+//            connection=new SqlConnection(connectionstring);//This is constructor which is used to initialize the class level variable
 
-        }
-        public void OpenConnection()
-        {
-            try
-            {
-                connection.Open();
-                Console.WriteLine("Database connection open Succesfully");
-            }
-            catch (Exception e) 
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-        public void CloseConnection()
-        {
-            if(connection.State==System.Data.ConnectionState.Closed)
-            {
-                connection.Close();
-                Console.WriteLine("Database connection Closed");
-            }
-        }
-    }
-    internal class ParameterizedDataBaseConnection
-    {
-        static void Main(string[] args)
-        {
-            string connstring = "Server=SURESH\\MSSQLSERVER1;Initial Catalog=Oops;Integrated Security=True;";
-            DataBaseConnection db= new DataBaseConnection(connstring);
-            db.OpenConnection();
-            db.CloseConnection();
-        }
-    }
-}
+//        }
+//        public void OpenConnection()
+//        {
+//            try//This is try catch block which is used to handle the exception if any exception occurs while opening the connection
+//            {
+//                connection.Open();
+//                Console.WriteLine("Database connection open Succesfully");
+//            }
+//            catch (Exception e) //This is catch block which is used to catch the exception if any exception occurs while opening the connection
+//            {
+//                Console.WriteLine(e.Message);
+//            }
+//        }
+//        public void CloseConnection()
+//        {
+//            if(connection.State==System.Data.ConnectionState.Open)
+//            {
+//                connection.Close();
+//                Console.WriteLine("Database connection Closed");
+//            }
+//        }
+//    }
+//    internal class ParameterizedDataBaseConnection
+//    {
+//        static void Main(string[] args)
+//        {
+//            string connstring = "Server=SURESH\\MSSQLSERVER1;Initial Catalog=Oops;Integrated Security=True;";//This is connection string which is used to connect to the database
+//            DataBaseConnection db= new DataBaseConnection(connstring);//This is object of the DataBaseConnection class which is used to call the methods of the class
+//            db.OpenConnection();//This is method which is used to open the connection
+//            db.CloseConnection();//This is method which is used to close the connection
+//        }
+//    }
+//}
