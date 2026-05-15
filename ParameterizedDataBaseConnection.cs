cc6e0785f1d077c5e6cea42819e832e670d7c8cb
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-15 11:32:47 +0530

    commit

---
+++ ParameterizedDataBaseConnection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOops
+{
+    class DataBaseConnection
+    {
+        string connectionstring;
+        SqlConnection connection;
+        public DataBaseConnection(string connstring)
+        {
+            connectionstring = connstring;
+            connection=new SqlConnection(connectionstring);
+
+        }
+        public void OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Database connection open Succesfully");
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void CloseConnection()
+        {
+            if(connection.State==System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+                Console.WriteLine("Database connection Closed");
+            }
+        }
+    }
+    internal class ParameterizedDataBaseConnection
+    {
+        static void Main(string[] args)
+        {
+            string connstring = "Server=SURESH\\MSSQLSERVER1;Initial Catalog=Oops;Integrated Security=True;";
+            DataBaseConnection db= new DataBaseConnection(connstring);
+            db.OpenConnection();
+            db.CloseConnection();
+        }
+    }
+}
--- ParameterizedStudentd.cs
+++ ParameterizedStudentd.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpOops
-{
-    class Student
-    {
-        int Sid;
-        string Sname;
-        double Sfee;
+//namespace CSharpOops
+//{
+//    class Student
+//    {
+//        int Sid;
+//        string Sname;
+//        double Sfee;
 
-        public Student()
-        {
-            Sid = 1;
-            Sname = "Suresh";
-            Sfee = 40000;
-        }
-        public Student(int sid,string sname,double sfee)
-        {
-            Sid = sid;
-            Sname = sname;
-            Sfee = sfee;
-        }
-        public void DisplayStudent()
-        {
-            Console.WriteLine("Student id is:" + Sid);
-            Console.WriteLine("Student Name is:"+Sname);
-            Console.WriteLine("Stuedent fee is:" + Sfee);
-        }
-    }
-    internal class ParameterizedStudentd
-    {
-        static void Main(string[] args)
-        {
-            {
-                Student s = new Student();
-                s.DisplayStudent();
-                Student stud = new Student(102,"Sai",50000);
-                stud.DisplayStudent();
-            }
+//        public Student()
+//        {
+//            Sid = 1;
+//            Sname = "Suresh";
+//            Sfee = 40000;
+//        }
+//        public Student(int sid,string sname,double sfee)
+//        {
+//            Sid = sid;
+//            Sname = sname;
+//            Sfee = sfee;
+//        }
+//        public void DisplayStudent()
+//        {
+//            Console.WriteLine("Student id is:" + Sid);
+//            Console.WriteLine("Student Name is:"+Sname);
+//            Console.WriteLine("Stuedent fee is:" + Sfee);
+//        }
+//    }
+//    internal class ParameterizedStudentd
+//    {
+//        static void Main(string[] args)
+//        {
+//            {
+//                Student s = new Student();
+//                s.DisplayStudent();
+//                Student stud = new Student(102,"Sai",50000);
+//                stud.DisplayStudent();
+//            }
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
