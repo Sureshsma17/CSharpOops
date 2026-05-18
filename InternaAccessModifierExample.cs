5ac3b35b338ab402b5cef88fe46dfd65e165f0d3
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-18 11:38:59 +0530

    commit

--- InternaAccessModifierExample.cs
+++ InternaAccessModifierExample.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpOops
-{
-    class MyClass
-    {
-        internal int a;
+//namespace CSharpOops
+//{
+//    class MyClass
+//    {
+//        internal int a;
 
-        public MyClass()
-        {
-            a = 10;
-        }
-        public void Display()
-        {
-            Console.WriteLine("value of a is:" + a);
+//        public MyClass()
+//        {
+//            a = 10;
+//        }
+//        public void Display()
+//        {
+//            Console.WriteLine("value of a is:" + a);
 
-        }
+//        }
         
-    }
-    class NewClass
-    {
-        public void Method()
-        {
-            MyClass obj = new MyClass();
-            Console.WriteLine(obj.a);
-        }
+//    }
+//    class NewClass
+//    {
+//        public void Method()
+//        {
+//            MyClass obj = new MyClass();
+//            Console.WriteLine(obj.a);
+//        }
 
-    }
-    internal class InternaAccessModifierExample
-    {
-        static void Main(string[] args)
-        {
-            MyClass obj = new MyClass();
-            obj.Display();
-            NewClass obj1 = new NewClass();
-            obj1.Method();
-        }
-    }
-}
+//    }
+//    internal class InternaAccessModifierExample
+//    {
+//        static void Main(string[] args)
+//        {
+//            MyClass obj = new MyClass();
+//            obj.Display();
+//            NewClass obj1 = new NewClass();
+//            obj1.Method();
+//        }
+//    }
+//}
---
+++ ProtectedAccessModifierExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOops
+{
+    class myclass
+    {
+        protected int a = 10;
+
+        public void Method()
+        {
+            Console.WriteLine("value of a is:" + a);
+        }
+    }
+    class newclass : myclass
+    {
+        public void Method2()
+        {
+            Console.WriteLine("a value is:" + a);
+        }
+
+
+    }
+    internal class ProtectedAccessModifierExample
+    {
+        static void Main(string[] args)
+        {
+            newclass obj = new newclass();
+            obj.Method();
+            obj.Method2(); 
+        }
+    }
+}
