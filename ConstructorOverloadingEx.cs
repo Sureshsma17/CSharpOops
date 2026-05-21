96f0ec9e933efa24e84c0d91a529cffb3bd39000
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-21 15:04:29 +0530

    commit

---
+++ ConstructorOverloadingEx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOops
+{
+    class Student
+    {
+        int sid;
+        string name;
+        string college;
+
+        public Student()
+        {
+            sid = 101;
+            name = "RAM";
+            college = "JNTU";
+        }
+
+        public Student(int sid, string name, string college)
+        {
+            this.sid = sid;
+            this.name = name;
+            this.college = college;
+        }
+        public Student(int sid,string name)
+        {
+            this.sid = sid;
+            this.name = name;
+            college = "Not Provided";
+        }
+
+        public void DisplayStudent()
+        {
+            Console.WriteLine("Student ID " + sid);
+            Console.WriteLine("Student Name is "+name);
+            Console.WriteLine("Student Collehe name is:" + college);
+        }
+    }
+
+    internal class ConstructorOverloadingEx
+    {
+        static void Main(string[] args)
+        {
+            Student stuobj = new Student();
+            stuobj.DisplayStudent();
+
+            Student stuparam = new Student(102, "Sai", "Anurag University");
+            stuparam.DisplayStudent();
+
+            Student param = new Student(103, "Akash", "CBIT");
+            param.DisplayStudent();
+
+            Student pstu = new Student(104, "Suresh");
+            pstu.DisplayStudent();
+        }
+    }
+    
+    
+}
--- EmployeeDeatailsEndUserUsingArray.cs
+++ EmployeeDeatailsEndUserUsingArray.cs
@@ -1,65 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpOops
-{
-    class Employee
-    {
-        public int Id;
-        public string Name;
-        public string Department;
-        public double Salary;
+//namespace CSharpOops
+//{
+//    class Employee
+//    {
+//        public int Id;
+//        public string Name;
+//        public string Department;
+//        public double Salary;
 
-        public Employee(int Id, string Name, string Department, double Salary)
-        {
-            this.Id = Id;
-            this.Name = Name;
-            this.Department = Department;
-            this.Salary = Salary;
-        }
-        public void Display()
-        {
-            Console.WriteLine("Employee Id: " + Id);
-            Console.WriteLine("Employee Name: " + Name);
-            Console.WriteLine("Employee Department: " + Department);
-            Console.WriteLine("Employee Salary: " + Salary);
-        }
-    }
-    internal class EmployeeDeatailsEndUserUsingArray
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter Number of Employees");
-            int n = Convert.ToInt32(Console.ReadLine());
+//        public Employee(int Id, string Name, string Department, double Salary)
+//        {
+//            this.Id = Id;
+//            this.Name = Name;
+//            this.Department = Department;
+//            this.Salary = Salary;
+//        }
+//        public void Display()
+//        {
+//            Console.WriteLine("Employee Id: " + Id);
+//            Console.WriteLine("Employee Name: " + Name);
+//            Console.WriteLine("Employee Department: " + Department);
+//            Console.WriteLine("Employee Salary: " + Salary);
+//        }
+//    }
+//    internal class EmployeeDeatailsEndUserUsingArray
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter Number of Employees");
+//            int n = Convert.ToInt32(Console.ReadLine());
 
-            Employee[] employees = new Employee[n];//array of employee class type
+//            Employee[] employees = new Employee[n];//array of employee class type
 
-            for (int i = 0; i < n; i++)//loop to get employee details
-            {
-                Console.WriteLine("\n Enter Employee Details");
+//            for (int i = 0; i < n; i++)//loop to get employee details
+//            {
+//                Console.WriteLine("\n Enter Employee Details");
 
-                Console.WriteLine("Enter Employee ID:");
-                int id = Convert.ToInt32(Console.ReadLine());
+//                Console.WriteLine("Enter Employee ID:");
+//                int id = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter Employee Name:");
-                string name = Console.ReadLine();
+//                Console.WriteLine("Enter Employee Name:");
+//                string name = Console.ReadLine();
 
-                Console.WriteLine("Enter Employee Department:");
-                string department = Console.ReadLine();
+//                Console.WriteLine("Enter Employee Department:");
+//                string department = Console.ReadLine();
 
-                Console.WriteLine("Enter Employee Salary:");
-                double salary = Convert.ToDouble(Console.ReadLine());
+//                Console.WriteLine("Enter Employee Salary:");
+//                double salary = Convert.ToDouble(Console.ReadLine());
 
-                employees[i] = new Employee(id, name, department, salary);//creating object of employee class and storing in array
-            }
-            Console.WriteLine("Employee Details are");
-            for (int i = 0; i < employees.Length; i++)
-            {
-                employees[i].Display();
-            }
-        }
-    }
-}
+//                employees[i] = new Employee(id, name, department, salary);//creating object of employee class and storing in array
+//            }
+//            Console.WriteLine("Employee Details are");
+//            for (int i = 0; i < employees.Length; i++)
+//            {
+//                employees[i].Display();
+//            }
+//        }
+//    }
+//}
