b97cb1c07eab26a8a722b93b673dc2f8af300b55
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-21 11:04:02 +0530

    commit

--- ThisKeywordExample.cs
+++ ThisKeywordExample.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpOops
-{
-    class newclass
-    {
-        int a;
-        int b;
+//namespace CSharpOops
+//{
+//    class newclass
+//    {
+//        int a;
+//        int b;
 
-        public newclass(int a,int b)
-        {
-            this.a = a;
-            this.b = b;
+//        public newclass(int a,int b)
+//        {
+//            this.a = a;
+//            this.b = b;
 
-        }
-        public void Add()
-        {
-            int c = a + b;
-            Console.WriteLine("Sum of a and b is:"+c);
-        }
-    }
-    internal class ThisKeywordExample
-    {
-        static void Main(string[] args)
-        {
-            newclass obj = new newclass(10, 20);
-            obj.Add();
-        }
-    }
-}
+//        }
+//        public void Add()
+//        {
+//            int c = a + b;
+//            Console.WriteLine("Sum of a and b is:"+c);
+//        }
+//    }
+//    internal class ThisKeywordExample
+//    {
+//        static void Main(string[] args)
+//        {
+//            newclass obj = new newclass(10, 20);
+//            obj.Add();
+//        }
+//    }
+//}
