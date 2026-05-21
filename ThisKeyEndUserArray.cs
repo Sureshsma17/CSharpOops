f8c476d88cd19cc7573490673c40db3ba59b0232
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-21 12:48:58 +0530

    commit

---
+++ ThisKeyEndUserArray.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharpOops
+//{
+//    class Student
+//    {
+//        int sid;
+//        string sname;
+
+//        public Student(int sid, string sname)
+//        {
+//            this.sid = sid;
+//            this.sname = sname;
+//        }
+//        public void Display()
+//        {
+//            Console.WriteLine("Student ID id is:"+ sid);
+//            Console.WriteLine("Styudent Name is:"+ sname);
+//            Console.WriteLine("--------------------------");
+//        }
+        
+
+//    }
+//    internal class ThisKeyEndUserArray
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter Number of Students");
+//            int n=Convert.ToInt32(Console.ReadLine());
+
+//            Student[] students = new Student[n];//array of student class type
+
+//            for (int i = 0; i < n; i++)//loop to get student details
+//            {
+//                Console.WriteLine("\n Enter Student Details");
+
+//                Console.WriteLine("Enter Student ID:");
+//                int id= Convert.ToInt32(Console.ReadLine());
+
+//                Console.WriteLine("Enter Student Name:");
+//                string name= Console.ReadLine();
+
+//                students[i]= new Student(id, name);//creating object of student class and storing in array
+//            }
+//            Console.WriteLine("Student Deatails are");
+//            for(int i = 0;i < students.Length; i++)
+//            {
+//                students[i].Display();
+//            }
+//        }
+//    }
+//}
--- ThisKeyStudent.cs
+++ ThisKeyStudent.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpOops
-{
-    class Student
-    {
-        int sid;
-        string sname;
+//namespace CSharpOops
+//{
+//    class Student
+//    {
+//        int sid;
+//        string sname;
 
-        public Student(int sid,string sname)
-        {
-            this.sid = sid;
-            this.sname = sname;
-        }
-        public void Display()
-        {
-            Console.WriteLine($"The Student ID is: {sid},Student Name is :{sname}");
-        }
-    }
-    internal class ThisKeyStudent
-    {
-        static void Main(string[] args)
-        {
-            Student objstu = new Student(101, "Sai");
-            objstu.Display();
-        }
-    }
-}
+//        public Student(int sid,string sname)
+//        {
+//            this.sid = sid;
+//            this.sname = sname;
+//        }
+//        public void Display()
+//        {
+//            Console.WriteLine($"The Student ID is: {sid},Student Name is :{sname}");
+//        }
+//    }
+//    internal class ThisKeyStudent
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student objstu = new Student(101, "Sai");
+//            objstu.Display();
+//        }
+//    }
+//}
