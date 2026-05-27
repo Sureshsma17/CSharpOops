344e7f3f65ff9f6d1378f66e51b1bb67cbaecab2
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-27 11:49:00 +0530

    commit

--- Encapsulation_Ex2pin.cs
+++ Encapsulation_Ex2pin.cs
@@ -1,54 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpOops
-{
-    class Account
-    {
-        private double balance;
-        private int pin=1234;
-        public void Deposit(double amount)
-        {
-            if (amount > 0)
-            {
-                balance += amount;
-            }
+//namespace CSharpOops
+//{
+//    class Account
+//    {
+//        private double balance;
+//        private int pin=1234;
+//        public void Deposit(double amount)
+//        {
+//            if (amount > 0)
+//            {
+//                balance += amount;
+//            }
 
-        }
-        public void Withdraw(double amount,int enterpin)
-        {
-            if (enterpin == pin)
-            {
-                if (amount <= balance)
-                {
-                    balance -= amount;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid PIN. Access denied.");
-            }
+//        }
+//        public void Withdraw(double amount,int enterpin)
+//        {
+//            if (enterpin == pin)
+//            {
+//                if (amount <= balance)
+//                {
+//                    balance -= amount;
+//                }
+//            }
+//            else
+//            {
+//                Console.WriteLine("Invalid PIN. Access denied.");
+//            }
 
-        }
-        public double GetBalance()
-        {
+//        }
+//        public double GetBalance()
+//        {
             
-            return balance;
+//            return balance;
 
-        }
-    }
-        internal class Encapsulation_Ex2pin
-    {
-        static void Main(string[] args)
-        {
-            Account acc = new Account();
-            acc.Deposit(5000);
-            acc.Withdraw(2000,123);
-            acc.GetBalance();
-            Console.WriteLine("Balance: " + acc.GetBalance());
-        }
-    }
-}
+//        }
+//    }
+//        internal class Encapsulation_Ex2pin
+//    {
+//        static void Main(string[] args)
+//        {
+//            Account acc = new Account();
+//            acc.Deposit(5000);
+//            acc.Withdraw(2000,1234);
+//            acc.GetBalance();
+//            Console.WriteLine("Balance: " + acc.GetBalance());
+//        }
+//    }
+//}
