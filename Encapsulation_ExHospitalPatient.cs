443512c0643cf278856039338b6c6b64bdc37e15
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-27 12:11:28 +0530

    commit

---
+++ Encapsulation_ExHospitalPatient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOops
+{
+    class PatientDetails
+    {
+        private string Name;
+        private int Age;
+        private string MedicalHistory;
+
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public int age
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+
+        public string medicalHistory
+        {
+            get { return MedicalHistory; }
+            set { MedicalHistory = value; }
+        }
+
+        public PatientDetails(string name, int age, string medicalHistory)
+        {
+            Name = name;
+            Age = age;
+            MedicalHistory = medicalHistory;
+        }
+    }
+    internal class Encapsulation_ExHospitalPatient
+    {
+        static void Main(string[] args)
+        {
+            PatientDetails patient = new PatientDetails("ABC", 30, "No known allergies");
+            Console.WriteLine("Patient Name: " + patient.name);
+            Console.WriteLine("Patient Age: " + patient.age);
+            Console.WriteLine("Patient Medical History: " + patient.medicalHistory);
+        }
+    }
+}
---
+++ Encapsupulation_BankExample.cs
@@ -0,0 +1,52 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharpOops
+//{
+//    class BankAccount
+//    {
+//        private double balance;
+//        private string pin;
+
+//        public double Balance
+//        {
+//            get { return balance; }
+//            set { 
+//                if(value>=0) 
+//                balance = value; }
+//        }
+//        public void Deposit(double amount)
+//        {
+//            if (amount > 0)
+//            {
+//                balance += amount;
+//            }
+//        }
+//        public void Withdraw(double amount, string enteredpin)
+//        {
+//            if (enteredpin == pin && amount <= Balance)
+//            {
+//                Balance -= amount;
+//            }
+//        }
+//        public BankAccount(double initialBalance, string accountpin)
+//        {
+//            Balance = initialBalance;
+//            pin = accountpin;
+//        }
+//        internal class Encaspulation_BankExample
+//        {
+//            static void Main(string[] args)
+//            {
+//                BankAccount acc = new BankAccount(10000, "1234");
+//                acc.Deposit(5000);
+//                acc.Withdraw(2000,"1234");
+//                acc.Withdraw(2000, "9999");
+//                Console.WriteLine("Final Balance is: " + acc.Balance);
+//            }
+//        }
+//    }
+//}
