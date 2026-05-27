ba6a9861c29792ca3e43961fad89727b0d001f9b
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-27 10:51:07 +0530

    commit

---
+++ Encapsulation_Example.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOops
+{
+    class Account
+    {
+        private double balance;
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+            }
+            
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount<=balance)
+            {
+                balance -= amount;
+            }
+            
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+    internal class Encapsulation_Example
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            acc.Deposit(5000);
+            acc.Withdraw(2000);
+            Console.WriteLine("Balance: " + acc.GetBalance());
+        }
+    }
+}
