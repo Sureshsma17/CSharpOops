d88634a862de9f36c131018d88097fa84c879b4b
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-05-28 10:49:01 +0530

    commit

---
+++ VitualMethod_Example.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOops
+{
+    class myclass
+    {
+        public void Display()
+        {
+            Console.WriteLine("Welcome to Oops");
+
+        }
+        public virtual void Show()
+        {
+            Console.WriteLine("Show is Calling");
+        }
+    }
+    class dc : myclass
+    {
+        public void Message()
+        {
+            Console.WriteLine("Message is Calling");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("This is Overridden Method");
+        }
+    }
+    internal class VitualMethod_Example
+    {
+        static void Main(string[] args)
+        {
+            myclass obj = new myclass();
+            obj.Display();
+            obj.Show();
+
+            dc objdc = new dc();
+            objdc.Message();
+            objdc.Show();
+        }
+    }
+}
